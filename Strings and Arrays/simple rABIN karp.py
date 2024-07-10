8aaa91017da77119145bdd4fa61a5d6a28b044a0
Abin (אבין ברגיס) <abinvarghese90@gmail.com>
2024-07-10 06:51:35 +0100

    simple rABIN karp

--- Strings and Arrays/simple rABIN karp.py
+++ Strings and Arrays/simple rABIN karp.py
@@ -8,6 +8,23 @@ class RabinKarp:
 
     def calculate_hash(self, string):
         """Calculate hash for a given string"""
+        """
+        The conceptual formula for the hash. For "ABC" with base 256, it would indeed be:
+        A * 256² + B * 256¹ + C * 256⁰
+        This code is actually implementing the same concept, just in an iterative manner. Let's break it down step by step for "ABC":
+        1. Start with hash_value = 0
+        2. For 'A':
+         hash_value = (256 * 0 + ord('A')) % prime
+         This is equivalent to: A * 256⁰
+        3. For 'B':
+         hash_value = (256 * (A * 256⁰) + ord('B')) % prime
+         This is equivalent to: A * 256¹ + B * 256⁰
+        4. For 'C':
+         hash_value = (256 * (A * 256¹ + B * 256⁰) + ord('C')) % prime
+         This is equivalent to: A * 256² + B * 256¹ + C * 256⁰
+        As you can see, after processing each character, we're effectively shifting the previous value left by one position (multiplying by 256) and then adding the new character.
+        The `% prime` operation is performed at each step to keep the numbers manageable and to distribute the hash values more evenly.
+        """
         hash_value = 0
         for char in string:
             hash_value = (self.base * hash_value + ord(char)) % self.prime
