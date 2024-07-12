6feb8fe5df48a414e82ab55def74cbdc1365a55f
Abin (אבין ברגיס) <abinvarghese90@gmail.com>
2024-07-12 11:03:12 +0100

    !

---
+++ Strings and Arrays/14 longest common prefix.py
@@ -0,0 +1,15 @@
+# 14. Longest common prefix [easy]
+
+import unittest
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        pass
+
+class TestLongestCommonPrefix(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+if __name__ == "__main__":
+    unittest.main()
