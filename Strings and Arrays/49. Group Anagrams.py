c1ae8ccc6b7a70f7a7a0e2f581cdcab6e7168b26
Abin (אבין ברגיס) <abinvarghese90@gmail.com>
2024-08-01 14:10:18 +0100

    49

--- Strings and Arrays/49. Group Anagrams.py
+++ Strings and Arrays/49. Group Anagrams.py
@@ -26,6 +26,7 @@ Constraints:
 0 <= strs[i].length <= 100
 strs[i] consists of lowercase English letters.
 """
+from typing import *
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
