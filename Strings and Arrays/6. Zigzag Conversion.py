e9f82931496f8c65597f8df9cfbdc9144c0eda85
Abin (אבין ברגיס) <abinvarghese90@gmail.com>
2024-08-10 18:35:29 +0100

    6 updated

--- Strings and Arrays/6. Zigzag Conversion.py
+++ Strings and Arrays/6. Zigzag Conversion.py
@@ -1,4 +1,8 @@
 # medium
+
+# this is a very interesting question on its own. 
+# you could think of it as an encoding algorithm (which at the moment can't be decoded? lol)
+
 """
 The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
 
